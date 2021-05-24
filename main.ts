19bd12cbcae9263c05a2b3cfa5652375e309191e
Alicia-28 <83964114+Alicia-28@users.noreply.github.com>
2021-05-24 10:19:37 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -6,44 +6,123 @@ enum ActionKind {
 }
 function walk_left () {
     walk_l = animation.createAnimation(ActionKind.Walking, 150)
-    walk_l.addAnimationFrame(img`
-        . . . f f f . . . f f f . . . . 
-        . . . f 4 4 . . . 4 4 f . . . . 
-        . . . f 4 d f f f d 4 f . . . . 
-        . . . . . f 4 4 4 f . . . . . . 
-        . . . . f 1 1 4 1 1 f . . . . . 
-        . . . . f e 1 4 e 1 f . . . . . 
-        . . . . f 4 4 3 4 4 f . . . . . 
-        . . . . f 4 4 4 4 4 f . . . . . 
-        . . . . . f 4 e 4 f . . . . . . 
-        . . . . 4 4 4 4 4 4 4 . . . . . 
-        . . . . 4 . 4 4 4 . 4 . . . . . 
-        . . . . f . 4 4 4 . f . . . . . 
-        . . . . . . 4 4 4 . . . . . . . 
-        . . . . . f 4 . 4 . . . . . . . 
-        . . . . f 4 4 . 4 f . . . . . . 
-        . . . . . . . . 4 4 f . . . . . 
-        `)
-    walk_l.addAnimationFrame(img`
-        . . . f f f . . . f f f . . . . 
-        . . . f 4 4 . . . 4 4 f . . . . 
-        . . . f 4 d f f f d 4 f . . . . 
-        . . . . . f 4 4 4 f . . . . . . 
-        . . . . f 1 1 4 1 1 f . . . . . 
-        . . . . f e 1 4 e 1 f . . . . . 
-        . . . . f 4 4 3 4 4 f . . . . . 
-        . . . . f 4 4 4 4 4 f . . . . . 
-        . . . . . f 4 e 4 f . . . . . . 
-        . . . . 4 4 4 4 4 4 4 . . . . . 
-        . . . . 4 . 4 4 4 . 4 . . . . . 
-        . . . . f . 4 4 4 . f . . . . . 
-        . . . . . . 4 4 4 . . . . . . . 
-        . . . . . . 4 . 4 f . . . . . . 
-        . . . . . f 4 . 4 4 f . . . . . 
-        . . . . f 4 4 . . . . . . . . . 
-        `)
-    animation.setAction(Mr_Rat, ActionKind.Walking)
-    animation.attachAnimation(Mr_Rat, walk_l)
+    animation.runImageAnimation(
+    Mr_Rat,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . f f f f f f b f f . . . . 
+        . . . f 3 3 3 3 3 3 3 b f . . . 
+        . . f e b 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e e 3 b e 3 3 3 3 f . . 
+        . f 3 3 e e e f f 3 3 3 3 f . . 
+        . f 3 e e e f b f b b b b f . . 
+        . . f e 4 4 f 1 e b b b b f . . 
+        . . . f 4 4 4 4 f b b b b f f . 
+        . . . f e e e f f f b b b b f . 
+        . . . f d d d e 4 4 f b b f . . 
+        . . . f d d d e 4 4 e f f . . . 
+        . . f b d b d b e e b f . . . . 
+        . . f f 1 d 1 d 1 d f f . . . . 
+        . . . . f f b b f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . f e 3 3 3 3 3 3 3 b f . . . 
+        . . f 3 3 3 3 3 3 3 3 3 b f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e e 3 b e 3 3 3 3 f . . 
+        . f 3 3 e e e f f 3 3 3 3 f . . 
+        . . f e e e f b f b b b b f f . 
+        . . . e 4 4 f 1 e b b b b b f . 
+        . . . f 4 4 4 4 f b b b b b f . 
+        . . . f d d d e 4 4 b b b f . . 
+        . . . f d d d e 4 4 f f f . . . 
+        . . f d d d b b e e b b f . . . 
+        . . f b d 1 d 1 d d b f . . . . 
+        . . . f f f b b f f f . . . . . 
+        `],
+    500,
+    true
+    )
+}
+function up () {
+    go_up = animation.createAnimation(ActionKind.Walking, 150)
+    animation.runImageAnimation(
+    Mr_Rat,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        f c b b b b b b b b b b b b c f 
+        f b b b b b b b b b b b b b b f 
+        . f c c b b b b b b b b c c f . 
+        . . e 4 c f f f f f f c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        f c b b b b b b b b b b b b f . 
+        f b b b b b b b b b b b b c f . 
+        f f b b b b b b b b b b c f . . 
+        . f c c c f f f f f f f e c . . 
+        . . . f b b d b d d e 4 4 e . . 
+        . . . f f 1 1 d 1 d e e f . . . 
+        . . . . . f b b f f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        f c b b b b b b b b b b b b c f 
+        f b b b b b b b b b b b b b b f 
+        . f c c b b b b b b b b c c f . 
+        . . e 4 c f f f f f f c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        . f b b b b b b b b b b b b c f 
+        . f c b b b b b b b b b b b b f 
+        . . f c b b b b b b b b b b f f 
+        . . c e f f f f f f f c c c f . 
+        . . e 4 4 e d d b d b b f . . . 
+        . . . f e e d 1 d 1 1 f f . . . 
+        . . . . . f f f b b f . . . . . 
+        `],
+    500,
+    true
+    )
 }
 function fly () {
     if (Seagull.isHittingTile(CollisionDirection.Right)) {
@@ -55,6 +134,83 @@ function fly () {
 function jump () {
     Mr_Rat.ay += 0
 }
+function down () {
+    go_down = animation.createAnimation(ActionKind.Walking, 150)
+    animation.runImageAnimation(
+    Mr_Rat,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f 3 3 3 3 3 3 3 3 f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b d d d d b f e e f . 
+        . . e 4 c d d d d d d c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f 3 3 3 3 3 3 3 3 f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . f e 3 3 3 3 3 3 3 3 3 3 e f . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f b 3 f f e e e e f f 3 b f . 
+        f f b b f b f e e f b f b b f f 
+        f b b b e 1 f 4 4 f 1 e b b b f 
+        . f b b e e 4 4 4 4 4 f b b f . 
+        . . f 4 4 4 e d d d b f e f . . 
+        . . f e 4 4 e d d d d c 4 e . . 
+        . . . f e e d d b d b b f e . . 
+        . . . f f 1 d 1 d 1 1 f f . . . 
+        . . . . . f f f b b f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f 3 3 3 3 3 3 3 3 f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b d d d d b f e e f . 
+        . . e 4 c d d d d d d c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f 3 3 3 3 3 3 3 3 f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . f e 3 3 3 3 3 3 3 3 3 3 e f . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f b 3 f f e e e e f f 3 b f . 
+        f f b b f b f e e f b f b b f f 
+        f b b b e 1 f 4 4 f 1 e b b b f 
+        . f b b f 4 4 4 4 4 e e b b f . 
+        . . f e f b d d d e 4 4 4 f . . 
+        . . e 4 c d d d d e 4 4 e f . . 
+        . . e f b b d b d d e e f . . . 
+        . . . f f 1 1 d 1 d 1 f f . . . 
+        . . . . . f b b f f f . . . . . 
+        `],
+    500,
+    true
+    )
+}
 function win_or_lose () {
     if (Mr_Rat.tileKindAt(TileDirection.Left, assets.tile`tile8`)) {
         game.over(true)
@@ -64,68 +220,72 @@ function win_or_lose () {
 }
 function walk_right () {
     walk_r = animation.createAnimation(ActionKind.Walking, 150)
-    walk_r.addAnimationFrame(img`
-        . . . f f f . . . f f f . . . . 
-        . . . f 4 4 . . . 4 4 f . . . . 
-        . . . f 4 d f f f d 4 f . . . . 
-        . . . . . f 4 4 4 f . . . . . . 
-        . . . . f 1 1 4 1 1 f . . . . . 
-        . . . . f 1 e 4 1 e f . . . . . 
-        . . . . f 4 4 3 4 4 f . . . . . 
-        . . . . f 4 4 4 4 4 f . . . . . 
-        . . . . . f 4 e 4 f . . . . . . 
-        . . . . 4 4 4 4 4 4 4 . . . . . 
-        . . . . 4 . 4 4 4 . 4 . . . . . 
-        . . . . f . 4 4 4 . f . . . . . 
-        . . . . . . 4 4 4 . . . . . . . 
-        . . . . . . 4 . 4 f . . . . . . 
-        . . . . . f 4 . 4 4 f . . . . . 
-        . . . . f 4 4 . . . . . . . . . 
-        `)
-    walk_r.addAnimationFrame(img`
-        . . . f f f . . . f f f . . . . 
-        . . . f 4 4 . . . 4 4 f . . . . 
-        . . . f 4 d f f f d 4 f . . . . 
-        . . . . . f 4 4 4 f . . . . . . 
-        . . . . f 1 1 4 1 1 f . . . . . 
-        . . . . f 1 e 4 1 e f . . . . . 
-        . . . . f 4 4 3 4 4 f . . . . . 
-        . . . . f 4 4 4 4 4 f . . . . . 
-        . . . . . f 4 e 4 f . . . . . . 
-        . . . . 4 4 4 4 4 4 4 . . . . . 
-        . . . . 4 . 4 4 4 . 4 . . . . . 
-        . . . . f . 4 4 4 . f . . . . . 
-        . . . . . . 4 4 4 . . . . . . . 
-        . . . . . f 4 . 4 . . . . . . . 
-        . . . . f 4 4 . 4 f . . . . . . 
-        . . . . . . . . 4 4 f . . . . . 
-        `)
-    animation.setAction(Mr_Rat, ActionKind.Walking)
-    animation.attachAnimation(Mr_Rat, walk_r)
+    animation.runImageAnimation(
+    Mr_Rat,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . f b 3 3 3 3 3 3 3 f f . . 
+        . . f b 3 3 3 3 3 3 3 3 3 f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . . f 3 3 3 3 e b 3 e e 3 3 f . 
+        . . f 3 3 3 3 f f e e e 3 3 f . 
+        . . f b b b b f b f e e e f . . 
+        . . f b b b b e 1 f 4 4 e . . . 
+        . f f b b b b f 4 4 4 4 f . . . 
+        . f b b b b f f f e e e f . . . 
+        . . f b b f 4 4 e d d d f . . . 
+        . . . f f e 4 4 e d d d f . . . 
+        . . . . f b e e b d b d b f . . 
+        . . . . f f d 1 d 1 d 1 f f . . 
+        . . . . . . f f b b f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f f f . . . 
+        . . . f b 3 3 3 3 3 3 3 f f . . 
+        . . f b 3 3 3 3 3 3 3 3 3 f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . . f 3 3 3 3 e b 3 e e 3 3 f . 
+        . . f 3 3 3 3 f f e e e 3 3 f . 
+        . f f b b b b f b f e e e f . . 
+        . f b b b b b e 1 f 4 4 e . . . 
+        . f b b b b b f 4 4 4 4 f . . . 
+        . . f b b b 4 4 e d d d f . . . 
+        . . . f f f 4 4 e d d d f . . . 
+        . . . f b b e e b b d d d f . . 
+        . . . . f b d d 1 d 1 d b f . . 
+        . . . . . f f f b b f f f . . . 
+        `],
+    500,
+    true
+    )
 }
 let walk_r: animation.Animation = null
+let go_down: animation.Animation = null
+let go_up: animation.Animation = null
 let walk_l: animation.Animation = null
 let Seagull: Sprite = null
 let Mr_Rat: Sprite = null
 info.startCountdown(120)
 tiles.setTilemap(tilemap`level3`)
 Mr_Rat = sprites.create(img`
-    . . . f f f . . . f f f . . . . 
-    . . . f 4 4 . . . 4 4 f . . . . 
-    . . . f 4 d f f f d 4 f . . . . 
-    . . . . . f 4 4 4 f . . . . . . 
-    . . . . f 1 1 4 1 1 . . . . . . 
-    . . . . f 1 e 4 1 e f . . . . . 
-    . . . . f 4 4 3 4 4 f . . . . . 
-    . . . . f 4 4 4 4 4 f . . . . . 
-    . . . . . f 4 e 4 f . . . . . . 
-    . . . 4 4 4 4 4 4 4 4 4 . . . . 
-    . . . 4 . . 4 4 4 . . 4 . . . . 
-    . . . f . . 4 4 4 . . f . . . . 
-    . . . . . . 4 4 4 . . . . . . . 
-    . . . . . . 4 4 4 . . . . . . . 
-    . . . . . f 4 . 4 f . . . . . . 
-    . . . . f 4 4 . 4 4 f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . f f f f f f f f . . . . 
+    . . . f e 3 3 3 3 3 3 e f . . . 
+    . . f b 3 3 3 3 3 3 3 3 b f . . 
+    . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f 3 3 f f e e e e f f 3 3 f . 
+    . f b b f b f e e f b f b b f . 
+    . f b b e 1 f 4 4 f 1 e b b f . 
+    f f b b f 4 4 4 4 4 4 f b b f f 
+    f b b f f f e e e e f f f b b f 
+    . f e e f b d d d d b f e e f . 
+    . . e 4 c d d d d d d c 4 e . . 
+    . . e f b d b d b d b b f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f b b f f . . . . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(Mr_Rat)
 Mr_Rat.x = 6
@@ -154,12 +314,15 @@ Seagull.y = 120
 Seagull.setVelocity(68, 0)
 game.onUpdate(function () {
     fly()
+    jump()
     if (controller.left.isPressed()) {
         walk_left()
     } else if (controller.right.isPressed()) {
         walk_right()
     } else if (controller.up.isPressed()) {
-        jump()
+        up()
+    } else if (controller.down.isPressed()) {
+        down()
     }
     win_or_lose()
 })
